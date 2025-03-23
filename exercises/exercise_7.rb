70f4e021c5273cd8bdea0ccc32d0bef5bce68d3e
ArcaneCipher <desjardins.jarp@gmail.com>
2025-03-22 21:51:35 -0400

    Complete Exercise 7: Add model validations and demonstrate error handling

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,25 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+require_relative "../setup"
+require_relative "exercise_1"
+require_relative "exercise_2"
+require_relative "exercise_3"
+require_relative "exercise_4"
+require_relative "exercise_5"
+require_relative "exercise_6"
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter a store name: "
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+
+if store.errors.any?
+  puts "Store creation failed due to the following validation errors:"
+  store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+else
+  puts "Store '#{store.name}' created successfully."
+end
