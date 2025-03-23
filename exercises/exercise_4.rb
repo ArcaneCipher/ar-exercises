d2604f7c96bd8b2035ec6abd8272289f01ddafb0
ArcaneCipher <desjardins.jarp@gmail.com>
2025-03-22 21:31:21 -0400

    Complete Exercise 4: Query and display stores by apparel type

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,26 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+require_relative "../setup"
+require_relative "exercise_1"
+require_relative "exercise_2"
+require_relative "exercise_3"
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create([
+  {name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true},
+  {name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: true},
+  {name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true}
+])
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "The men's store #{store.name} makes #{store.annual_revenue} in revenue"
+end
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+@womens_stores.each do |store|
+  puts "The women's store #{store.name} makes #{store.annual_revenue} in revenue"
+end
